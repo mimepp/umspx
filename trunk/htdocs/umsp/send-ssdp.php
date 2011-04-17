73f348ab36ee4e4926fb13d14f48ac7b58a1dd3b
yut616 <yut616@af3e7cc0-368e-4ba0-230f-8a2ad97b9476>
2011-04-17 05:40:13 +0000

    1. add ssdp for rootdevice, uuid, devicetype, service CM, sevice CD in "send-ssdp.php" 2. remove send-ssdp-rootdevice.php

--- trunk/htdocs/umsp/send-ssdp.php
+++ trunk/htdocs/umsp/send-ssdp.php
@@ -35,21 +35,48 @@ function _myCheck() {
 	return $strSSDPResult;
 }
 
-function _sddpSend($frame, $delay=15, $host="239.255.255.250", $port=1900) {
-	$strHeader  = 'NOTIFY * HTTP/1.1' . "\r\n";
-	$strHeader .= 'HOST: 239.255.255.250:1900' . "\r\n";
-	$strHeader .= 'LOCATION: http://'.$_SERVER['SERVER_ADDR'].':'.$_SERVER['SERVER_PORT'].'/umsp/MediaServerServiceDesc.xml' . "\r\n";
-	$strHeader .= 'SERVER: MYFAKESERVER UDP 127' . "\r\n";
-	$strHeader .= 'CACHE-CONTROL: max-age=7200' . "\r\n";
-	$strHeader .= 'NT: urn:schemas-upnp-org:device:MediaServer:1' . "\r\n";
-	$strHeader .= 'NTS: ssdp:alive' . "\r\n";
-	$strHeader .= 'USN: uuid:badbabe1-6666-6666-6666-f00d00c0ffee::urn:schemas-upnp-org:device:MediaServer:1' . "\r\n". "\r\n";
-    $buf = $strHeader;
-    $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+function _udpSend($buf, $delay=15, $host="239.255.255.250", $port=1900) {
+	  $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
     socket_bind($socket, $_SERVER['SERVER_ADDR']);
     socket_sendto($socket, $buf, strlen($buf), 0, $host, $port);
     socket_close($socket);
     usleep($delay*1000);
+}
+
+function _sddpSend($frame, $delay=15, $host="239.255.255.250", $port=1900) {
+	$uuidStr = 'badbabe1-6666-6666-6666-f00d00c0ffee';
+	$strHeader  = 'NOTIFY * HTTP/1.1' . "\r\n";
+	$strHeader .= 'HOST: 239.255.255.250:1900' . "\r\n";
+	$strHeader .= 'LOCATION: http://'.$_SERVER['SERVER_ADDR'].':'.$_SERVER['SERVER_PORT'].'/umsp/MediaServerServiceDesc.xml' . "\r\n";
+	$strHeader .= 'SERVER: MYFAKESERVER UDP 127' . "\r\n";
+	$strHeader .= 'CACHE-CONTROL: max-age=7200' . "\r\n";	
+	$strHeader .= 'NTS: ssdp:alive' . "\r\n";
+	
+	$rootDevice = 'NT: upnp:rootdevice' . "\r\n";
+	$rootDevice .= 'USN: uuid:' . $uuidStr . '::upnp:rootdevice' . "\r\n". "\r\n";
+	
+	$buf = $strHeader . $rootDevice;
+	_udpSend($buf);
+	
+	$uuid = 'NT: uuid:' . $uuidStr . "\r\n";
+	$uuid .= 'USN: uuid:' . $uuidStr . "\r\n". "\r\n";
+	$buf = $strHeader . $uuid;
+	_udpSend($buf);
+	
+	$deviceType = 'NT: urn:schemas-upnp-org:device:MediaServer:1' . "\r\n";
+	$deviceType .= 'USN: uuid:' . $uuidStr . '::urn:schemas-upnp-org:device:MediaServer:1' . "\r\n". "\r\n";
+	$buf = $strHeader . $deviceType;
+	_udpSend($buf);	
+		
+	$serviceCM = 'NT: urn:schemas-upnp-org:service:ConnectionManager:1' . "\r\n";
+	$serviceCM .= 'USN: uuid:' . $uuidStr . '::urn:schemas-upnp-org:service:ConnectionManager:1' . "\r\n". "\r\n";
+	$buf = $strHeader . $serviceCM;
+	_udpSend($buf);	
+	
+	$serviceCD = 'NT: urn:schemas-upnp-org:service:ContentDirectory:1' . "\r\n";
+	$serviceCD .= 'USN: uuid:' . $uuidStr . '::urn:schemas-upnp-org:service:ContentDirectory:1' . "\r\n". "\r\n";
+	$buf = $strHeader . $serviceCD;
+	_udpSend($buf);	
 }
 ?>
 
