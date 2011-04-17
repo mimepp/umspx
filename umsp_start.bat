bbf3a7d130649874e001e6935491e25b53b1c9bd
yut616 <yut616@af3e7cc0-368e-4ba0-230f-8a2ad97b9476>
2011-04-17 05:40:13 +0000

    1. add ssdp for rootdevice, uuid, devicetype, service CM, sevice CD in "send-ssdp.php" 2. remove send-ssdp-rootdevice.php

--- umsp_start.bat
+++ umsp_start.bat
@@ -7,6 +7,5 @@ color 1f
 echo.
 echo.
 wget.exe http://192.168.1.3:7703/umsp/send-ssdp.php?btnSend=Submit -O - -q
-wget.exe http://192.168.1.3:7703/umsp/send-ssdp-rootdevice.php?btnSend=Submit -O - -q
 ping -n 5 127.0.0.1 >nul 2>nul
 goto start
